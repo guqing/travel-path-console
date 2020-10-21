37dca020949406bc518126bec7a4f799007ea8af
guqing <1484563614@qq.com>
2020-10-21 19:48:29 +0800

    feat: 新增布设卡口方案列表页

--- src/utils/leafletHelper.js
+++ src/utils/leafletHelper.js
@@ -9,3 +9,23 @@ export var CheckPointIcon = L.Icon.extend({
     iconAnchor: [13, 38]
   }
 })
+
+export var PresetIcon = L.Icon.extend({
+  options: {
+    iconUrl: require('@/assets/checkpoint/geekblue.svg'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    shadowAnchor: [14, 41],
+    iconSize: [25, 39],
+    iconAnchor: [13, 38]
+  }
+})
+
+export var DesignIcon = L.Icon.extend({
+  options: {
+    iconUrl: require('@/assets/checkpoint/red.svg'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    shadowAnchor: [14, 41],
+    iconSize: [25, 39],
+    iconAnchor: [13, 38]
+  }
+})
