5f1cc0c99a219b3ec78104de5bdcaf13ef042662
guqing <1484563614@qq.com>
2020-12-21 17:42:17 +0800

    fix: 修复404跳转

--- src/views/account/center/Index.vue
+++ src/views/account/center/Index.vue
@@ -5,9 +5,18 @@
         <a-card :bordered="false">
           <div class="account-center-avatarHolder">
             <div class="avatar" title="点击可以修改头像">
-              <a-upload name="file" :showUploadList="false" :beforeUpload="handleBeforeUpload">
+              <a-upload
+                name="file"
+                :showUploadList="false"
+                :beforeUpload="handleBeforeUpload"
+              >
                 <div class="ant-upload-preview" type="upload">
-                  <a-avatar size="large" icon="user" :src="avatar" class="avatar-preview" />
+                  <a-avatar
+                    size="large"
+                    icon="user"
+                    :src="avatar"
+                    class="avatar-preview"
+                  />
                 </div>
               </a-upload>
             </div>
@@ -17,10 +26,16 @@
           <div class="account-center-detail">
             <p>
               <a-icon type="crown" />
-              <a-tag v-for="(item, index) in userInfo.roleNames" :key="index">{{ item }}</a-tag>
+              <a-tag v-for="(item, index) in userInfo.roleNames" :key="index">{{
+                item
+              }}</a-tag>
             </p>
-            <p v-if="userInfo.email" title="邮箱地址"><a-icon type="mail" />{{ userInfo.email }}</p>
-            <p v-if="userInfo.mobile" title="手机号"><a-icon type="phone" />{{ userInfo.mobile }}</p>
+            <p v-if="userInfo.email" title="邮箱地址">
+              <a-icon type="mail" />{{ userInfo.email }}
+            </p>
+            <p v-if="userInfo.mobile" title="手机号">
+              <a-icon type="phone" />{{ userInfo.mobile }}
+            </p>
             <p v-if="userInfo.createTime" title="账号年龄">
               <a-icon type="calendar" />{{ userInfo.createTime | dateToNow }}
             </p>
@@ -32,46 +47,39 @@
 
           <div class="social-account">
             <div class="socail-account-title">关联第三方账号</div>
-            <a-list class="social-account-list" item-layout="horizontal" :data-source="socailAccounts">
-              <a-list-item slot="renderItem" slot-scope="item, index" :key="index">
-                <a slot="actions" v-if="item.isConnected" @click="handleUnbindSocial(item.provider)">取消关联</a>
-                <a slot="actions" v-else @click="handleBindSocialAccount(item.provider)">关联</a>
+            <a-list
+              class="social-account-list"
+              item-layout="horizontal"
+              :data-source="socailAccounts"
+            >
+              <a-list-item
+                slot="renderItem"
+                slot-scope="item, index"
+                :key="index"
+              >
+                <a
+                  slot="actions"
+                  v-if="item.isConnected"
+                  @click="handleUnbindSocial(item.provider)"
+                >
+                  取消关联
+                </a>
+                <a
+                  slot="actions"
+                  v-else
+                  @click="handleBindSocialAccount(item.provider)"
+                >
+                  关联
+                </a>
                 <a-list-item-meta>
-                  <a slot="title" href="https://www.antdv.com/">{{ item.name }}</a>
+                  <a slot="title" href="https://www.antdv.com/">{{
+                    item.name
+                  }}</a>
                   <a-avatar slot="avatar" :src="item.logo" />
                   <div>{{ item.isConnected ? '已关联' : '未关联' }}</div>
                 </a-list-item-meta>
               </a-list-item>
             </a-list>
-          </div>
-          <a-divider :dashed="true" />
-
-          <div class="account-center-tags">
-            <div class="tagsTitle">标签</div>
-            <div>
-              <template v-for="(tag, index) in tags">
-                <a-tooltip v-if="tag.length > 20" :key="tag" :title="tag">
-                  <a-tag :key="tag" :closable="index !== 0" :close="() => handleTagClose(tag)">
-                    {{ `${tag.slice(0, 20)}...` }}
-                  </a-tag>
-                </a-tooltip>
-                <a-tag v-else :key="tag" :closable="index !== 0" :close="() => handleTagClose(tag)">{{ tag }}</a-tag>
-              </template>
-              <a-input
-                v-if="tagInputVisible"
-                ref="tagInput"
-                type="text"
-                size="small"
-                :style="{ width: '78px' }"
-                :value="tagInputValue"
-                @change="handleInputChange"
-                @blur="handleTagInputConfirm"
-                @keyup.enter="handleTagInputConfirm"
-              />
-              <a-tag v-else @click="showTagInput" style="background: #fff; borderstyle: dashed">
-                <a-icon type="plus" />New Tag
-              </a-tag>
-            </div>
           </div>
 
           <a-divider :dashed="true" />
@@ -102,7 +110,13 @@ import BaseSetting from './page/BaseSetting'
 import PasswordPage from './page/PasswordPage'
 import AvatarModal from './page/AvatarModal'
 import { mapGetters } from 'vuex'
-import { listSupportSocail, socialLoginApi, bindSocial, listUserConnectedSocail, unbindSocial } from '@/api/login'
+import {
+  listSupportSocail,
+  // socialLoginApi,
+  bindSocial,
+  listUserConnectedSocail,
+  unbindSocial
+} from '@/api/login'
 import { getSocailInfo } from '@/utils/socailInfo'
 import userApi from '@/api/user'
 
@@ -122,9 +136,6 @@ export default {
       },
       userConnectedSocail: [],
       socailAccounts: [],
-      tags: ['旧街凉风', '坐拥百态', '醉酒入梦', '晚风抚人', '无人及你'],
-      tagInputVisible: false,
-      tagInputValue: '',
       tabListNoTitle: [
         {
           key: 'base',
@@ -157,13 +168,14 @@ export default {
   },
   methods: {
     handleBindSocialAccount(oauthType) {
-      const url = `${socialLoginApi}/${oauthType.toLowerCase()}/bind`
-      window.open(
-        url,
-        'Bind Social Account',
-        `height=${this.page.height},width=${this.page.width}, top=10%, left=10%, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no`
-      )
-      window.addEventListener('message', this.resolveBindResult, false)
+      this.$message.warning('暂不支持绑定')
+      // const url = `${socialLoginApi}/${oauthType.toLowerCase()}/bind`
+      // window.open(
+      //   url,
+      //   'Bind Social Account',
+      //   `height=${this.page.height},width=${this.page.width}, top=10%, left=10%, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no`
+      // )
+      // window.addEventListener('message', this.resolveBindResult, false)
     },
     resolveBindResult(e) {
       this.$log.debug('解析得到绑定结果:', e.data, e.data.data)
@@ -228,32 +240,6 @@ export default {
     },
     handleTabChange(key, type) {
       this[type] = key
-    },
-    handleTagClose(removeTag) {
-      const tags = this.tags.filter(tag => tag !== removeTag)
-      this.tags = tags
-    },
-    // tag
-    showTagInput() {
-      this.tagInputVisible = true
-      this.$nextTick(() => {
-        this.$refs.tagInput.focus()
-      })
-    },
-    handleInputChange(e) {
-      this.tagInputValue = e.target.value
-    },
-    handleTagInputConfirm() {
-      const inputValue = this.tagInputValue
-      let tags = this.tags
-      if (inputValue && !tags.includes(inputValue)) {
-        tags = [...tags, inputValue]
-      }
-      Object.assign(this, {
-        tags,
-        tagInputVisible: false,
-        tagInputValue: ''
-      })
     }
   }
 }
--- src/views/exception/404.vue
+++ src/views/exception/404.vue
@@ -1,5 +1,9 @@
 <template>
-  <a-result status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
+  <a-result
+    status="404"
+    title="404"
+    sub-title="Sorry, the page you visited does not exist."
+  >
     <template #extra>
       <a-button type="primary" @click="toHome">
         Back Home
@@ -13,7 +17,7 @@ export default {
   name: 'Exception404',
   methods: {
     toHome() {
-      this.$router.push({ path: '/' })
+      this.$router.push({ path: '/user/login' })
     }
   }
 }
